497f5f58c4a3854e50188d6a3f3a31f3e458e701
paigeerasmussen <paigeerasmussen@gmail.com>
2025-11-01 10:23:04 -0600

    updates

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const year = document.querySelector("#year");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
