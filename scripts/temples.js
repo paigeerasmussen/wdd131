3d9a3cea442f83a3624543919904ce8869a86e5b
paigeerasmussen <paigeerasmussen@gmail.com>
2025-11-07 09:04:06 -0700

    updates

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+const year = document.querySelector("#year");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const hamButton = document.querySelector("#menu")
+const navigation = document.querySelector(".navigation")
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
