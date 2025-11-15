72ab3994117967ca9db357daf0ef17bbed24539e
paigeerasmussen <paigeerasmussen@gmail.com>
2025-11-15 10:38:40 -0700

    updates

--- scripts/place.js
+++ scripts/place.js
@@ -8,9 +8,11 @@ let temperature = 49;
 let windSpeed = 4;
 let windChillValue = 0;
 
+function calculateWindChill(temperature, windSpeed) {
+    return 35.74 + 0.6215 * temperature - 35.75 * windSpeed ** 0.16 + 0.4275 * temperature * windSpeed ** 0.16;
+}
+
 if (temperature <= 50 && windSpeed > 3) {
-    calculateWindChill = (temperature, windSpeed) =>
-        35.74 + 0.6215 * temperature - 35.75 * windSpeed ** 0.16 + 0.4275 * temperature * windSpeed ** 0.16;
     windChillValue = calculateWindChill(temperature, windSpeed).toFixed(2) + "°F";
 }
 else {
@@ -18,3 +20,18 @@ else {
 }
 
 document.getElementById('windChillValue').textContent = windChillValue
+
+function handleResize() {
+    if (window.innerWidth < 500) {
+        const smallImage = document.getElementById('smallImage');
+        const headerSection = document.querySelector('header');
+        headerSection.appendChild(smallImage);
+    }
+    if (window.innerWidth >= 500) {
+        const smallImage = document.getElementById('smallImage');
+        const mainSection = document.querySelector('main');
+        mainSection.appendChild(smallImage);
+    }
+}
+
+window.addEventListener('resize', handleResize);
