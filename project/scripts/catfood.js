7d8b24bb40924f03b38193c147b4d3eda5f14823
paigeerasmussen <paigeerasmussen@gmail.com>
2025-12-03 23:29:51 -0700

    updates

--- project/scripts/catfood.js
+++ project/scripts/catfood.js
@@ -3,3 +3,12 @@ const today = new Date();
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const hamButton = document.querySelector("#menu")
+const navigation = document.querySelector(".navigation")
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
